fb0b386d27f78ca34c0d568779aa67916f36b1c5
Johnsmz <88699538+Johnsmz@users.noreply.github.com>
2024-06-03 10:35:44 +0000

    Update binaries.h

--- algorithms/binaries.h
+++ algorithms/binaries.h
@@ -46,21 +46,6 @@ void merge_sort(int *arr, int len) {
 
 int binary_search(int *arr, int from, int to, int target) {
   if (to < from) {
-    return from;
-  }
-  int mid = (from + to) / 2;
-  if (arr[mid] == target) {
-    return mid;
-  }
-  if (arr[mid] > target) {
-    return binary_search(arr, from, mid - 1, target);
-  } else {
-    return binary_search(arr, mid + 1, to, target);
-  }
-}
-
-int binary_search_with_m1(int *arr, int from, int to, int target) {
-  if (to < from) {
     return -1;
   }
   int mid = (from + to) / 2;
@@ -71,5 +56,20 @@ int binary_search_with_m1(int *arr, int from, int to, int target) {
     return binary_search_with_m1(arr, from, mid - 1, target);
   } else {
     return binary_search_with_m1(arr, mid + 1, to, target);
+  }
+}
+
+int binary_search_insert(int *arr, int from, int to, int target) {
+  if (to < from) {
+    return from;
+  }
+  int mid = (from + to) / 2;
+  if (arr[mid] == target) {
+    return mid;
+  }
+  if (arr[mid] > target) {
+    return binary_search(arr, from, mid - 1, target);
+  } else {
+    return binary_search(arr, mid + 1, to, target);
   }
 }
