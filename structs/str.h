ae1e7361b874d7f660df072d9156a92f463b2401
Johnsmz <88699538+Johnsmz@users.noreply.github.com>
2025-08-20 15:09:03 +0800

    fix str.h notation

--- structs/str.h
+++ structs/str.h
@@ -5,26 +5,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define null NULL
+
 typedef char *string;
-typedef int size_l;
+
 typedef struct {
-    size_l len;
+    size_t len;
     char *str;
 } * str;
 
 str str_create(void) {
     str s = (str)malloc(sizeof(*s));
-    s->len = 0;
-    s->str = null;
+    s->len = 0x0;
+    s->str = NULL;
     return s;
 }
-str str_create_len(size_l len) {
-    if (len < 0) {
-        return null;
+str str_create_len(size_t len) {
+    if (len < 0x0) {
+        return NULL;
     }
     str s = (str)malloc(sizeof(*s));
-    s->len = 0;
+    s->len = 0x0;
     s->str = (string)calloc(len, sizeof(char));
     return s;
 }
@@ -35,15 +35,15 @@ str str_from_string(const string s) {
     memcpy(o->str, s, sizeof(char) * o->len);
     return o;
 }
-size_l str_len(str s) {
-    if (s == null) {
-        return 0;
+size_t str_len(str s) {
+    if (!s) {
+        return 0x0;
     }
     return s->len;
 }
 str str_sub(str s, int x, int y) {
-    if (x < 0 || y > s->len || x > y) {
-        return null;
+    if (x < 0x0 || y > s->len || x > y) {
+        return NULL;
     }
     str o = str_create_len(y - x);
     for (int i = x; i < y; i++) {
@@ -57,8 +57,8 @@ string str_to_string(str s) {
     return out;
 }
 str str_cat(str a, str b) {
-    if (a == null || b == null) {
-        return null;
+    if ( !a || !b ) {
+        return NULL;
     }
     str out = str_create_len(a->len + b->len);
     memcpy(out->str, a->str, sizeof(char) * a->len);
@@ -67,20 +67,20 @@ str str_cat(str a, str b) {
 }
 bool str_equal(str a, str b) {
     if (a->len != b->len) {
-        return 0;
+        return 0x0;
     }
     return !strncmp(a->str, b->str, a->len);
 }
 
 struct{
-    str (*create) (void);
-    str (*create_len) (size_l len);
-    str (*from_string) (const string s);
-    size_l (*length) (str s);
-    str (*sub) (str s, int from, int to);
-    string (*to_string) (str s);
-    str (*cat) (str a, str b);
-    bool (*equal) (str a, str b);
+    str     (*create)       (void);
+    str     (*create_len)   (size_t len);
+    str     (*from_string)  (const string s);
+    size_t  (*length)       (str s);
+    str     (*sub)          (str s, int from, int to);
+    string  (*to_string)    (str s);
+    str     (*cat)          (str a, str b);
+    bool    (*equal)        (str a, str b);
 } String = {str_create, str_create_len,str_from_string,str_len,str_sub,str_to_string,str_cat,str_equal};
 
 #endif
