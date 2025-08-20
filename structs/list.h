3d9a820a8ec61bffe30215988d40899f02fa37a1
Johnsmz <88699538+Johnsmz@users.noreply.github.com>
2025-08-20 15:44:14 +0800

    list.h renovate

--- structs/list.h
+++ structs/list.h
@@ -1,188 +1,77 @@
-#ifndef __JOHNSMZ_LIST_H__
-#define __JOHNSMZ_LIST_H__
+#ifndef __LIST_H__
+#define __LIST_H__
 
 #include <errno.h>
-#include <stdio.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-typedef void *element;
+typedef void * element;
 
-typedef struct {
+struct __LIST_H_NODE__ {
     element data;
-    void *next, *prev, *head;
-} *linode;
+    struct __LIST_H_NODE__ * prev;
+    struct __LIST_H_NODE__ * next;
+};
 
 typedef struct {
     size_t size;
-    linode head, tail;
-} *list;
+    struct __LIST_H_NODE__ * head;
+    struct __LIST_H_NODE__ * tail;
+} * list;
 
-linode linode_create(list list, element data) {
-    linode node = (linode)malloc(sizeof(*node));
-    node->data = data;
-    node->head = list;
-    return node;
+list __LIST_H_CREATE__ (void) {
+    list out = (list)malloc(sizeof(*out));
+    out->size = 0x0;
+    out->head = (struct __LIST_H_NODE__*)malloc(sizeof(*(out->head)));
+    out->tail = (struct __LIST_H_NODE__*)malloc(sizeof(*(out->tail)));
+    out->head->data = NULL; out->head->prev = NULL; out->head->next = out->tail;
+    out->tail->data = NULL; out->tail->prev = out->head; out->tail->next = NULL;
+    return out;
 }
 
-list list_create(void) {
-    list li = (list)malloc(sizeof(*li));
-    li->size = 0;
-    linode head = linode_create(li, NULL), tail = linode_create(li, NULL);
-    li->head = head;
-    li->tail = tail;
-    head->next = tail;
-    tail->prev = head;
-    return li;
+size_t __LIST_H_SIZE__ (list l) { return l->size; }
+
+element __LIST_H_GET__ (list l, size_t i) {
+    if ( i < 0x0 || i >= l->size ) { errno = EINVAL; perror("List.get() => index out of bound"); exit(errno); }
+    struct __LIST_H_NODE__ * ptr = l->head->next; while (i--) { ptr = ptr->next; } return ptr->data;
 }
 
-size_t list_size(list list) { return list->size; }
-
-linode linode_get_node(linode node, int index) {
-    if (node == NULL) {
-        errno = 0x5;
-        perror("List: cannot get node");
-        exit(errno);
-        return NULL;
-    }
-    if (!index) {
-        return node;
-    }
-    if (index > 0) {
-        while (index--) {
-            node = (linode)node->next;
-        }
-        return node;
-    }
-    while (index++) {
-        node = (linode)node->prev;
-    }
-    return node;
+void __LIST_H_SET__ (list l, size_t i, element d) {
+    if ( i < 0x0 || i >= l->size ) { errno = EINVAL; perror("List.set() => index out of bound"); exit(errno); }
+    struct __LIST_H_NODE__ * ptr = l->head->next; while (i--) { ptr = ptr->next; } ptr->data = d;
 }
 
-element linode_get(linode node, int index) {
-    if (node == NULL) {
-        errno = 0x5;
-        perror("List: cannot get node");
-        exit(errno);
-        return NULL;
-    }
-    if (!index) {
-        return node;
-    }
-    if (index > 0) {
-        while (index--) {
-            node = (linode)node->next;
-        }
-        return node->data;
-    }
-    while (index++) {
-        node = (linode)node->prev;
-    }
-    return node->data;
+void __LIST_H_INSERT__ (list l, size_t i, element data) {
+    if ( i < 0x0 || i > l->size ) { errno = EINVAL; perror("List.insert() => index out of bound"); exit(errno); }
+    struct __LIST_H_NODE__ * ptr = l->head; while (i--) { ptr = ptr->next; }
+    struct __LIST_H_NODE__ * nw  = (struct __LIST_H_NODE__ *) malloc(sizeof(*nw));
+    nw->data = data; nw->prev = ptr; nw->next = ptr->next; ptr->next = nw; nw->next->prev = nw;
+    l->size++;
 }
 
-element list_get(list list, int index) {
-    if (list == NULL) {
-        errno = 0x5;
-        perror("List: list is NULL");
-        exit(errno);
-        return NULL;
-    }
-    if ((index >= 0 && index >= list->size) || (index < 0 && -index > list->size)) {
-        errno = 0xc;
-        perror("List: index out of bound");
-        exit(errno);
-        return NULL;
-    }
-    if (index >= 0) {
-        return linode_get(list->head, index + 1);
-    }
-    return linode_get(list->tail, index);
+void __LIST_H_REMOVE__ (list l, size_t i) {
+    if ( i < 0x0 || i >= l->size ) { errno = EINVAL; perror("List.remove() => index out of bound"); exit(errno); }
+    struct __LIST_H_NODE__ * ptr = l->head; while (i--) { ptr = ptr->next; }
+    struct __LIST_H_NODE__ * tar = ptr->next; ptr->next = tar->next; ptr->next->prev = ptr;
+    free(tar); l->size--;
 }
 
-void list_add(list list, int index, element data) {
-    if (index < 0 || index > list->size) {
-        errno = 0xc;
-        perror("List: index out of bound");
-        exit(errno);
-        return;
-    }
-    linode left = linode_get_node(list->head, index), right = (linode)left->next, new_node = linode_create(list, data);
-    new_node->prev = left;
-    left->next = new_node;
-    new_node->next = right;
-    right->prev = new_node;
-    list->size++;
-    return;
-}
-
-void list_remove(list list, int index) {
-    if ((index >= 0 && index >= list->size) || (index < 0 && -index > list->size)) {
-        errno = 0xc;
-        exit(errno);
-        return;
-    }
-    linode node = linode_get_node(list->head, index + 1), left = (linode)node->prev, right = (linode)node->next;
-    left->next = right;
-    right->prev = left;
-    list->size--;
-    return;
-}
-
-void list_set(list list, int index, element data) {
-    if ((index >= 0 && index >= list->size) || (index < 0 && -index > list->size)) {
-        errno = 0xc;
-        perror("List: index out of bound");
-        exit(errno);
-        return;
-    }
-    linode node = linode_get_node(list->head, index + 1), left = (linode)node->prev, right = (linode)node->next, new_node = linode_create(list, data);
-    new_node->prev = left;
-    left->next = new_node;
-    new_node->next = right;
-    right->prev = new_node;
-    return;
-}
-
-void list_free(list list) {
-    linode ptr = (linode)list->head->next, nxt = (linode)ptr->next;
-    for (int i = 0x0; i < list->size; i++) {
-        free(ptr);
-        ptr = nxt;
-        nxt = (linode)nxt->next;
-    }
-    free(list->head);
-    free(list->tail);
-    free(list);
-    return;
-}
-
-void list_purge(list list) {
-    linode ptr = (linode)list->head->next, nxt = (linode)ptr->next;
-    for (int i = 0x0; i < list->size; i++) {
-        if (ptr->data) {
-            free(ptr->data);
-        }
-        free(ptr);
-        ptr = nxt;
-        nxt = (linode)nxt->next;
-    }
-    free(list->head);
-    free(list->tail);
-    free(list);
-    return;
+void __LIST_H_DESTROY__ (list l) {
+    struct __LIST_H_NODE__ * ptr_1 = l->head, *ptr_2 = ptr_1->next;
+    while ((l->size)--) { free(ptr_1); ptr_1 = ptr_2; ptr_2 = ptr_2?ptr_2->next:NULL; }
+    if (ptr_2) { free(ptr_2); }
+    free(l);
 }
 
 struct {
-    list        (*create)       (void);
-    size_t      (*size)         (list list);
-    element     (*get)          (list list, int index);
-    void        (*add)          (list list, int index, element data);
-    void        (*remove)       (list list, int index);
-    void        (*set)          (list list, int index, element data);
-    void        (*free)         (list list);
-    void        (*purge)        (list list);
-} List = { list_create, list_size, list_get, list_add, list_remove, list_set, list_free, list_purge };
+    list    (*create)   (void);
+    size_t  (*size)     (list list);
+    element (*get)      (list list, size_t index);
+    void    (*set)      (list list, size_t index, element data);
+    void    (*insert)   (list list, size_t index, element data);
+    void    (*remove)   (list list, size_t index);
+    void    (*destroy)  (list list);
+} List = { __LIST_H_CREATE__, __LIST_H_SIZE__, __LIST_H_GET__, __LIST_H_SET__, __LIST_H_INSERT__, __LIST_H_REMOVE__, __LIST_H_DESTROY__ };
 
 #endif
